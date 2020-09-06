c6a2845fda4a30997b36b90ec8f81e66d3da9c99
Arun <arun.ranga@hotmail.ca>
2020-09-06 12:18:16 -0400

    layout

---
+++ src/store/modules/authentication.js
@@ -0,0 +1,19 @@
+
+const state = {
+    currentUser: null
+};
+
+const getters = {
+    currentUser: state => state.currentUser
+}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+  };
