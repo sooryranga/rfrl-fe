befecd4983b08c8e745982de2a762d9aa86a8751
Arun Ranganathan <arun.ranga@hotmail.ca>
2020-09-20 10:39:36 -0400

    use calendar app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <div id="app">
-    <div id="topbar">
-      <div class="nav left">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/questions">Ask Question</router-link> |
-        <router-link to="/tutors">Find Tutor</router-link> |
-        <router-link to="/about">About</router-link>
+  <div id="app" class="container h-100 w-100">
+    <div class="sticky-top row navbar navbar-default navbar-fixed-top align-items-center justify-content-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <div class="col-sm">
+        <router-link class="p-2" to="/">TutorMe</router-link>
       </div>
-      <div class="nav right">
+      <nav class="col-sm row my-2 my-md-0 mr-md-3 justify-content-end">
+        <router-link class="p-2 text-dark" to="/questions">Ask Question</router-link>
+        <router-link class="p-2 text-dark" to="/tutors">Find Tutor</router-link>
+        <router-link class="p-2 text-dark mr-3" to="/about">About</router-link>
         <div v-if="currentUser">
-            <router-link to="/"> {{currentUser.name}}</router-link> |
+            <router-link class="p-2 text-dark" to="/"> {{currentUser.name}}</router-link>
         </div>
-        <div v-else>
-            <router-link to="/signup">Sign Up</router-link> |
-            <router-link to="/login">Login</router-link>
+        <div class="d-flex align-items-center" v-else>
+            <router-link class="btn btn-dark p-2 mr-2" to="/signup">Sign Up</router-link>
+            <router-link class="btn btn-dark p-2" to="/login">Login</router-link>
         </div>
-      </div>
+      </nav>
     </div>
-    <router-view/>
+    <div class="row row-cols-1 h-75 align-middle">
+      <router-view class="col"/>
+    </div>
   </div>
 </template>
 
@@ -30,35 +32,19 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  min-height: 100%;
 }
-
-#nav {
-  padding: 30px;
+html, body {
+    height: 100%;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-#topbar {
-  display: flex;
-}
-.left {
-  float: left;
-}
-.right {
-  float: right;
+.sticky-top{
+  z-index:4
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import auth from './modules/authentication';
+import question from './modules/questions'; 
 
 // Load Vuex
 Vue.use(Vuex);
@@ -8,6 +9,7 @@ Vue.use(Vuex);
 // Create store
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    question
   }
 });
