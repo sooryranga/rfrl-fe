debcf95784c796db15989192f74150e8c67f198a
Arun Ranganathan <arun.ranga@hotmail.ca>
2020-09-27 17:29:36 -0400

    format files

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,12 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import {mapGetters} from 'vuex';
 
 export default {
   name: 'tutor',
-  computed: mapGetters(['currentUser'])
-}
+  computed: mapGetters(['currentUser']),
+};
 </script>
 
 <style scoped>
