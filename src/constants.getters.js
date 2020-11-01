0ae80c8dab78a9f83e2c18af92db23734e791597
Soory Ranganathan <soory.ranga@hotmail.com>
2020-11-01 10:34:17 -0500

    add profile

---
+++ src/constants.getters.js
@@ -0,0 +1,7 @@
+// GETTERS for user
+const GET_PROFILE = 'get_profile';
+
+export {
+  // GETTERS for user
+  GET_PROFILE,
+};
---
+++ src/store/modules/profile.js
@@ -0,0 +1,53 @@
+import {v1 as uuidv1} from 'uuid';
+
+import {PROFILE_PICTURE, NAME} from '@/constants.actions.js';
+import {
+  SET_PROFILE, SET_PROFILE_IMAGE, SET_NAME,
+} from '@/constants.mutations.js';
+
+
+const state ={
+  profile: {
+    id: uuidv1(),
+    name: null,
+    birthDay: null,
+    profileImage: null,
+    about: null,
+    education: {},
+    workExperience: {},
+    documentation: {},
+  },
+};
+
+const getters = {
+  currentProfile: (state) => state.profile,
+};
+
+const actions = {
+  [PROFILE_PICTURE]({commit}, image) {
+    commit(SET_PROFILE_IMAGE, image);
+  },
+  [NAME]({commit}, name) {
+    commit(SET_NAME, name);
+  },
+};
+
+const mutations = {
+  [SET_PROFILE](state, profile) {
+    state.profile = {...profile};
+  },
+  [SET_PROFILE_IMAGE](state, imageSrc) {
+    state.profile.profileImage = imageSrc;
+  },
+  [SET_NAME](state, name) {
+    state.profile.name = name;
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
