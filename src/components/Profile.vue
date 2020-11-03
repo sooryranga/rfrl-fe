6f9a75a3c5b54f691c77cd9f06cc3b7d07248232
Arun Ranganathan <arun.ranga@hotmail.ca>
2020-11-03 08:57:58 -0500

    fix profile router
    Signed-off-by: Arun Ranganathan <arun.ranga@hotmail.ca>

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="row">
-      <div class="col-7">
-        <div id="profileheader" class="shadow p-3 my-5 bg-white">
-	  <img class="profilePicture" v-bind:src="currentProfile.profileImage"/>
-          <div>
-            <h3>{{currentProfile.name}}</h3>
-            <h4 class="text-left text-uppercase"> About </h4>
-            <p>{{currentProfile.about}}</p>
-          </div>
+    <div class="col-7">
+      <div id="profileheader" class="shadow p-3 my-5 pb-5 bg-white">
+        <img class="profilePicture float-left" v-bind:src="currentProfile.profileImage"/>
+        <div>
+          <h3 class="test-left">{{currentProfile.name}}</h3>
+          <h4 class="text-left text-uppercase"> About </h4>
+          <p class="text-left">{{currentProfile.about}}</p>
         </div>
       </div>
+      <div id="education" class="shadow p-3 my-5 bg-white">
+        <h1 class="text-left"> Education </h1>
+        <ul class="text-left list-group list-group-flush">
+          <li
+            v-for="education in currentProfile.schooling" :key="education" class="list-group-item">
+            Vestibulum at eros
+          </li>
+        </ul>
+      </div>
+    </div>
       <div class="col">
       </div>
   </div>
@@ -25,5 +34,9 @@ export default {
 </script>
 
 <style>
-
+.profilePicture{
+  background-size: cover;
+  background-position: top center;
+  border-radius: 50%;
+}
 </style>
