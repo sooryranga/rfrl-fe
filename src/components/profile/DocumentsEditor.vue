2f75b5f3c49cf823665592278c10c9b192f3c84d
Soory Ranganathan <soory.ranga@hotmail.com>
2020-11-20 21:39:06 -0500

    wooow I added an editor
    Signed-off-by: Arun Ranganathan <arun.ranga@hotmail.ca>

--- src/components/profile/DocumentsEditor.vue
+++ src/components/profile/DocumentsEditor.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="modal">
-    <div class="modal-mask">
+    <div class="modal-mask text-left">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -10,11 +10,34 @@
             </button>
           </div>
           <div class="modal-body">
-            ...
+            <div class="row">
+              <div class="col-2">
+                <p>Name</p>
+              </div>
+              <div class="col">
+                <input class="w-100" v-model="name" placeholder="document name">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-2">
+                <p>Description</p>
+              </div>
+              <div class="col">
+                <textarea class="h-100 w-100" v-model="description" placeholder="add description"/>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-2">
+                <p>File</p>
+              </div>
+              <div class="col">
+                <input type="file" id="file" ref="file" v-on:change="handleFileUpload"/>
+              </div>
+            </div>
           </div>
           <div class="modal-footer">
             <button type="button" v-on:click="cancel" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" v-on:click="save" class="btn btn-primary">Save changes</button>
           </div>
         </div>
       </div>
@@ -23,21 +46,49 @@
 </template>
 
 <script>
+import {documentState} from '@/constants.state.js';
+
 export default {
   name: 'document-editor',
   props: {
-    id: String,
-    type: String,
-    src: String,
-    description: String,
+    documentprop: Object,
+  },
+  data: function() {
+    return {
+      id: null,
+      type: null,
+      description: null,
+      name: null,
+      src: null,
+      file: '',
+    };
   },
   methods: {
+    handleFileUpload: function() {
+      this.file = this.$refs.file.files[0];
+    },
     save: function() {
-      this.$emit('saveEvent');
+      // Save file to bucket and update src
+      const state = documentState();
+      state.name = this.name;
+      state.description = this.description;
+      state.src = this.src? this.src : 'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'; // eslint-disable-line
+      this.$emit(
+          'saveEvent',
+          state,
+      );
     },
     cancel: function() {
-      this.$emit('cancelEvent');
+      this.$emit(
+          'cancelEvent',
+      );
     },
+  },
+  mounted: function() {
+    this.id = this.documentprop.id;
+    this.description = this.documentprop.description;
+    this.name = this.documentprop.name;
+    this.src = this.documentprop.src;
   },
 };
 </script>
