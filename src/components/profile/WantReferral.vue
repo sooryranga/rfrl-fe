280361f90fed5c217fb03e2757b21be638def0df
Arun Ranganathan <arun.ranga@hotmail.ca>
2021-06-08 20:07:54 -0400

    feat: css for want referral

--- src/components/profile/WantReferral.vue
+++ src/components/profile/WantReferral.vue
@@ -1,27 +1,32 @@
 <template>
   <div id="want-referrals" class="shadow p-3 my-3 bg-white">
-    <div class="row">
-      <div class="col">
-        <h5 class="my-2" for="flexSwitchCheckDefault" >Want Referrals?</h5>
+    <div class="mx-4">
+      <div class="row">
+        <div class="col">
+          <h4 class="my-2 primary-color" for="flexSwitchCheckDefault" >Want Referrals?</h4>
+        </div>
+        <div class="col-2 my-auto">
+          <label class="switch">
+            <input
+              v-model="isLookingForReferral"
+              v-on:click="onClickReferralButton"
+              type="checkbox"
+              id="flexSwitchCheckDefault"/>
+              <div></div>
+          </label>
+        </div>
       </div>
-      <div class="col-2 my-auto mx-auto">
-        <input
-          v-model="isLookingForReferral"
-          v-on:click="onClickReferralButton"
-          type="checkbox"
-          id="flexSwitchCheckDefault">
-      </div>
-    </div>
-    <div v-if="isLookingForReferral" class="row w-100">
-      <div class="col w-100">
-        <multiselect
-        v-model="checkedCompanies" :options="companyList" :multiple="true"
-        :close-on-select="false" :clear-on-select="false"
-        :preserve-search="false" placeholder="Search for tags"
-        :select-label="''" :taggable="false" :deselect-label="''"
-        @remove="removeCompany" @select="addCompany" @close="multiselectClose"
-        @open="multiselectOpen"
-        label="name" track-by="name" :preselect-first="false" open-direction="bottom"/>
+      <div v-if="isLookingForReferral" class="row w-100">
+        <div class="col w-100">
+          <multiselect
+          v-model="checkedCompanies" :options="companyList" :multiple="true"
+          :close-on-select="false" :clear-on-select="false"
+          :preserve-search="false" placeholder="Search for tags"
+          :select-label="''" :taggable="false" :deselect-label="''"
+          @remove="removeCompany" @select="addCompany" @close="multiselectClose"
+          @open="multiselectOpen"
+          label="name" track-by="name" :preselect-first="false" open-direction="bottom"/>
+        </div>
       </div>
     </div>
   </div>
@@ -117,4 +122,47 @@ export default {
   background-color: #343a40;
   border-color: #343a40;
 }
+</style>
+
+<style scoped>
+
+/** Switch
+ -------------------------------------*/
+
+.switch input {
+  position: absolute;
+  opacity: 0;
+}
+
+/**
+ * 1. Adjust this to size
+ */
+
+.switch {
+  display: inline-block;
+  height: 1.13rem;
+  width: 2.2rem;
+  background: white;
+  border-radius: 1rem;
+  border: 1px solid var(--clr-gray-4);
+}
+
+.switch div {
+  height: 1rem;
+  width: 1rem;
+  border-radius: 1rem;
+  background: #FFF;
+  box-shadow: 0 0.1rem 0.3rem rgba(0,0,0,0.3);
+  -webkit-transition: all 300ms;
+     -moz-transition: all 300ms;
+          transition: all 300ms;
+}
+
+.switch input:checked + div {
+  -webkit-transform: translate3d(100%, 0, 0);
+     -moz-transform: translate3d(100%, 0, 0);
+          transform: translate3d(100%, 0, 0);
+          background-color: var(--clr-primary);
+}
+
 </style>
