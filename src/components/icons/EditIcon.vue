f9afc681468b19a9920c0d0cedfcac486ae61b96
Arun Ranganathan <arun.ranga@hotmail.ca>
2021-06-24 20:20:01 -0400

    feat: new tutoring scheduler look

---
+++ src/components/icons/EditIcon.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- eslint-disable vue/max-len -->
+  <svg xmlns="http://www.w3.org/2000/svg"
+    :width="size"
+    :height="size"
+    viewBox="0 0 256 256"
+    :aria-labelledby="iconName"
+    role="presentation"
+    id="icon"
+    ref="icon"
+    :class="iconClass"
+  >
+    <title
+      :id="iconName"
+      lang="en"
+    >{{ iconName }} icon</title>
+    <g>
+      <rect width="256" height="256" fill="none"/>
+      <path class="icon-stroke" :stroke="iconColor" d="M96,216H48a8,8,0,0,1-8-8V163.31371a8,8,0,0,1,2.34315-5.65686l120-120a8,8,0,0,1,11.3137,0l44.6863,44.6863a8,8,0,0,1,0,11.3137Z" fill="none"  stroke-linecap="round" stroke-linejoin="round"/>
+      <line class="icon-stroke" :stroke="iconColor" x1="136" y1="64" x2="192" y2="120" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
+    </g>
+  </svg>
+  <!-- eslint-enable -->
+</template>
+
+<script>
+import IconBaseMixinVue from './IconBaseMixin.vue';
+export default {
+  name: 'edit-icon',
+  data() {
+    return {
+      iconName: 'edit-icon',
+    };
+  },
+  mixins: [IconBaseMixinVue],
+};
+</script>
+
+<style scoped src="@/css/icons.css">
+</style>
