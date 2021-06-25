0c1d38e33b19861239e0b3287c5e6f832080646a
Soory Ranganathan <soory.ranga@hotmail.com>
2021-06-25 08:12:18 -0400

    feat: question selector working

--- src/components/questions/QuestionsSelector.vue
+++ src/components/questions/QuestionsSelector.vue
@@ -1,33 +1,28 @@
 <template>
-  <div class="h-100" v-if="questions">
-    <div class="row mb-2" style="height:4rem;">
+  <div class="h-100" v-if="questions" id="question-selector">
+    <div class="row" id="header-box">
       <div class="col-auto me-auto">
         <div>
-          <h4 class="m-0 primary-color">Questions</h4>
-          <p class="m-0">{{questions.length}} found</p>
+          <h1 id="title">Questions</h1>
+          <p>{{questions.length}} found</p>
         </div>
       </div>
-      <div class="col-auto" v-on:click="openEditor">
+      <div class="col-auto" v-on:click="openEditor" id="add-btn-container">
         <button class="no-styling-button">
-          <span class="material-icons" id="add-question-button">
-            add
-          </span>
+          <add-icon :iconColor="'white'" id="add-question-btn"></add-icon>
         </button>
       </div>
     </div>
     <ul class="list-group list-group-flush border-top">
-      <li class="list-group-item p-0 question-item"
-        style="height:6rem"
+      <li class="list-group-item question-item"
         v-for="(question, index) in questions"
         v-bind:key="index">
-        <div v-on:click="routeToQuestion(index)">
-          <div>
-            <p class="m-0 h6 px-0 question-name">{{shortenTitle(question.title)}}</p>
-            <p class="m-0 mb-4">{{question.from.firstName}} {{question.from.lastName[0]}}</p>
-            <p class="m-0">
-            <small>{{timeAgoFormat(question.createdAt)}} · {{question.applicants}} Applicants</small>
-            </p>
-          </div>
+        <div v-on:click="routeToQuestion(index)" class="question-container">
+          <p class="h6 px-0 question-name">{{shortenTitle(question.title)}}</p>
+          <p class="m-0 mb-4 question-client">{{question.from.firstName}} {{question.from.lastName[0]}}</p>
+          <p class="m-0 question-meta">
+          <small>{{timeAgoFormat(question.createdAt)}} · {{question.applicants}} Applicants</small>
+          </p>
         </div>
       </li>
       <infinite-loading @infinite="infiniteHandler">
@@ -48,7 +43,7 @@ import {mapActions, mapGetters} from 'vuex';
 import en from 'javascript-time-ago/locale/en';
 import TimeAgo from 'javascript-time-ago';
 import InfiniteLoading from 'vue-infinite-loading';
-
+import {AddIcon} from '@/components/icons';
 
 TimeAgo.addDefaultLocale(en);
 const timeAgo = new TimeAgo('en-US');
@@ -57,6 +52,7 @@ export default {
   name: 'QuestionsSelector',
   components: {
     InfiniteLoading,
+    AddIcon,
   },
   computed: {
     ...mapGetters('questions', ['questions']),
@@ -104,6 +100,42 @@ export default {
 </script>
 
 <style scroped>
+p {
+  color: white;
+}
+
+#header-box{
+  padding-top: 3.5rem;
+}
+
+#add-btn-container{
+  width: 3.5rem;
+  height:3.5rem;
+  flex-direction: column;
+  display:flex;
+  flex-wrap: nowrap;
+  justify-content:space-around;
+  align-items: center;
+}
+
+#add-question-btn{
+  font-size: 2.5rem;
+}
+
+#title{
+  font-size: 3rem;
+  font-weight: 400;
+}
+
+#question-selector{
+  width:380px;
+  background-color: var(--clr-primary);
+  color: white;
+  font-size:1.1rem;
+  padding-left: 1rem;
+  padding-right: 1.5rem;
+}
+
 #add-question-button{
   font-size:2rem;
   color: var(--clr-primary-lightest)
@@ -113,13 +145,42 @@ export default {
   color: var(--clr-primary-dark)
 }
 
+.question-container{
+  position: relative;
+  height:100%;
+}
+
 .question-name{
   font-size: 1.1rem;
+  font-weight: 400;
+  margin: 0;
 }
+
+.question-meta{
+  font-size: 0.8rem;
+  font-weight: 300;
+  margin: 0;
+  position:absolute;
+  bottom: 0;
+}
+
+.question-client{
+  font-size: 0.8rem;
+  font-weight: 300;
+  margin: 0;
+}
+
 .question-item{
   cursor: pointer;
+  background-color: rgba(0,0,0,0);
+  height:8rem;
+  border-color:white;
+  padding-left: 0;
+  padding-bottom: 0.3rem;
+  padding-right: 0;
+  padding-top: 0.3rem;
 }
-.question-item:hover .question-name{
-  color: var(--clr-primary-dark)
+.question-item:hover{
+  background-color: var(--clr-primary-light);
 }
 </style>
