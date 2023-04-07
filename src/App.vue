83e8d846390f8c13a9b9ba0628aad169cd6bf2e7
Arun <soory.ranga@hotmail.com>
2023-04-07 15:37:19 -0400

    remove unwanted files

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="topbar">
+      <div class="nav left">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/ask-question">Ask Question</router-link> |
+        <router-link to="find-tutor">Find Tutor</router-link> |
+        <router-link to="about">About</router-link>
+      </div>
+      <div class="nav right">
+        <div v-if="user">
+            <router-link to="/"></router-link> |
+        </div>
+        <div v-else>
+            <tab>Sign Up</tab>
+            <tab>
+        </div>
+      </div>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import WebCam from "../src";
-
+import { mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'HelloWorld',
+  computed: {
+    ...mapGetters([
+      'getUser'
+    ])
   }
 }
 </script>
@@ -24,6 +40,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
