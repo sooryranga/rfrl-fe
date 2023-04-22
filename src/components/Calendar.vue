055f75bbebf2673a31322adf2557acc746d05127
Soory Ranganathan <soory.ranga@hotmail.com>
2023-04-22 10:39:36 -0400

    use calendar app

---
+++ src/components/Calendar.vue
@@ -0,0 +1,43 @@
+<template>
+<div class='container'>
+  <div class='container h-100 mt-0'>
+    <div class='row h-100'>
+      <div class='col col-3 p-0'>
+        <div>
+          <p> {{$route.params.user_id}} </p>
+        </div>
+      </div>
+      <div class='col'>
+        <v-app id="dayspan" v-cloak>
+          <calendar-extension :calendar="calendar"></calendar-extension>
+        </v-app>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { Calendar } from 'dayspan'
+
+export default {
+  name: 'calendar',
+  data: function () {
+    return {
+      calendar: Calendar.weeks()
+    }
+  },
+  computed: mapGetters(['currentUser']),
+  methods:{
+  }
+}
+</script>
+
+<style scoped>
+.selectable{list-style-type: none;}
+.selectablerows{ border-top: 0px; border-left: 0px; border-right: 0px;}
+.selectablerows.ui-selecting { background: #93969a; }
+.selectablerows.ui-selected { border-bottom:0px }
+.scheduled{background: #3a3b3d; color:white}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import DaySpanVuetify from 'dayspan-vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
+
+
+Vue.use(Vuetify);
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+Vue.use(DaySpanVuetify, {
+  methods: {
+    getDefaultEventColor: () => '#1976d2'
+  }
+});
 
 Vue.config.productionTip = false
 
