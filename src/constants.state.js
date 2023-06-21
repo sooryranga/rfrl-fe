3dd0293fb6515d3aebe17213a46f765353ac386a
Soory Ranganathan <soory.ranga@hotmail.com>
2023-06-21 09:51:12 -0400

    profile editors
    Signed-off-by: Arun Ranganathan <arun.ranga@hotmail.ca>

--- src/constants.state.js
+++ src/constants.state.js
@@ -15,6 +15,52 @@ const profileState = () => {
   };
 };
 
+const documentState = () => {
+  return {
+    id: null,
+    type: null,
+    src: null,
+    description: null,
+  };
+};
+
+const educationState = () => {
+  return {
+    id: null,
+    institution: null,
+    degree: null,
+    fieldOfStudy: null,
+    start: null,
+    end: null,
+    institutionLogo: null,
+  };
+};
+
+const tutoredStudentsState = () => {
+  return {
+    profileId: null,
+    name: null,
+    lastTutoredDate: null,
+    image: null,
+  };
+};
+
+const tutorReview = () => {
+  return {
+    id: null,
+    studentImage: null,
+    studentName: null,
+    title: null,
+    createdDate: null,
+    stars: null,
+    description: null,
+  };
+};
+
 export {
   profileState,
+  documentState,
+  educationState,
+  tutoredStudentsState,
+  tutorReview,
 };
