e9ab1264fc56c07af195fdf51b9c443a790f9e2c
Soory Ranganathan <soory.ranga@hotmail.com>
2023-11-18 22:45:00 -0500

    WIP: Work on code edtior changes

---
+++ src/components/code/CodeTerminal.vue
@@ -0,0 +1,21 @@
+<template>
+  <vue-command :commands="commands" />
+</template>
+
+<script>
+import VueCommand, {createStdout} from 'vue-command';
+import 'vue-command/dist/vue-command.css';
+
+export default {
+  name: 'code-terminal',
+  components: {
+    VueCommand,
+  },
+
+  data: () => ({
+    commands: {
+      'hello-world': () => createStdout('Hello world'),
+    },
+  }),
+};
+</script>
