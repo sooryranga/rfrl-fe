68a876ed1631ddc76c5fa113ec881b4d767227fc
Soory Ranganathan <soory.ranga@hotmail.com>
2024-02-03 19:58:58 -0500

    feat: type of users look

--- src/components/register/TypeOfUser.vue
+++ src/components/register/TypeOfUser.vue
@@ -1,14 +1,27 @@
 <template>
-<div class="row h-100">
-  <div class="col h-100">
-    <button type="button" class="btn btn-outline-dark btn-lg" v-on:click="setTutor">
-      Looking to tutor
-    </button>
+<div class="row h-100 w-100 p-0 m-0">
+  <loading :active.sync="isLoading"/>
+  <div class="col h-100 p-0">
+    <img src="@/assets/pexels-djordje-petrovic-2102416.jpg" id="feature-img"/>
   </div>
-  <div class="col h-100">
-    <button type="button" class="btn btn-outline-dark btn-lg" v-on:click="goNext">
-      Looking to learn
-    </button>
+  <div class="col h-100 p-0">
+    <transition name="fade">
+      <div v-if="showError" class="alert alert-danger fade-in fixed-current-top" role="alert">
+        {{error}}
+      </div>
+    </transition>
+    <div id="parent">
+      <div class="referrer">
+        <button class="primary-btn primary-btn-dark h-100 w-100" v-on:click="setTutor">
+          Be a Refferer
+        </button>
+      </div>
+      <div class="referrer">
+        <button class="primary-btn primary-btn-dark h-100 w-100" v-on:click="goNext">
+          Get Referred
+        </button>
+      </div>
+    </div>
   </div>
 </div>
 </template>
@@ -43,6 +56,42 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.btn{
+  width: 400px;
+}
 
+#parent{
+  display: flex;
+  justify-content: space-around;
+  height: 100%;
+  flex-direction:column;
+  align-items: center;
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+}
+
+#feature-img{
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+
+.fixed-current-top {
+  position: absolute;
+  top: 1rem;
+  width: calc(100% - 2rem);
+  margin-left: 1rem;
+  margin-right: 1rem;
+  z-index: 123;
+}
+
+.referrer{
+  flex: 0 0 10%;
+  width: 50%;
+  max-width: 30rem;
+  height: 30%;
+  max-height: 30rem;
+  font-size: 1.5rem;
+}
 </style>
